c553a9346a192922dda5c8665282833493d19871
rib-bruno <ribeirobrunosouza@hotmail.com>
2023-03-21 18:15:08 -0300

    unit tests e testando components do Android; before e after

--- app/src/androidTest/java/com/example/testcase/ResourceCompareTest.kt
+++ app/src/androidTest/java/com/example/testcase/ResourceCompareTest.kt
@@ -3,6 +3,8 @@ package com.example.testcase
 import android.content.Context
 import androidx.test.core.app.ApplicationProvider
 import com.google.common.truth.Truth.assertThat
+import org.junit.After
+import org.junit.Before
 
 import org.junit.Test
 
@@ -12,10 +14,20 @@ class ResourceCompareTest {
 
     private lateinit var resourceCompare : ResourceCompare //inicializar em cada test para que eles tenham a própria instância
 
+    @Before
+    fun setup(){
+        //escrever a lógica que queremos executar antes da execução de cada testcase
+        resourceCompare = ResourceCompare()
+    }
+
+    @After
+    fun teardown() {
+        //ex abrir e fechar o room database
+    }
+
     @Test
     fun stringResourceSameAsGivenString_returnsTrue() {
-        resourceCompare = ResourceCompare()
-        //obtendo o objeto context no diretório de testes
+      //  resourceCompare = ResourceCompare() //obtendo o objeto context no diretório de testes
         val context = ApplicationProvider.getApplicationContext<Context>()
         val result = resourceCompare.isEqual(context, R.string.app_name, "TestCase")
         assertThat(result).isTrue()
@@ -23,8 +35,7 @@ class ResourceCompareTest {
 
     @Test
     fun stringResourceDifferentAsGivenString_returnsFalse() {
-        resourceCompare = ResourceCompare()
-        //obtendo o objeto context no diretório de testes
+      //  resourceCompare = ResourceCompare() //obtendo o objeto context no diretório de testes
         val context = ApplicationProvider.getApplicationContext<Context>()
         val result = resourceCompare.isEqual(context, R.string.app_name, "Hello")
         assertThat(result).isFalse()
